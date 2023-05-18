902fa0ffc8adb87b98abead58b0757a46383700c
AbhishekPatil1231 <133991456+AbhishekPatil1231@users.noreply.github.com>
2023-05-18 22:03:45 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,16 @@
+print("Enter your name")
+name = input()
+if name == "Abhi":
+  print("Enter your age")
+  age = int(input())
+  if age == 20:
+    print("Enter your password")
+    password = input()
+    if password == "Abhi123":
+      print("welcome,Master Abhi")
+    else:
+      print("Access Denied,You Are Not My Master!!!")
+  else:
+    print("You Are Not My Master!")
+else:
+  print("Access Denied MF")
